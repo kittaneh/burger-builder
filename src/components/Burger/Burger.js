2fbd7f1d57de2bb632c2ae3a4f60fc8f6ca2f336
Yahya Daoud <kittaneh@gmail.com>
2020-07-25 21:45:51 +0300

    complete

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,27 +3,23 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-
-const burger = (props) => {
-
-    let tranfromedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey + 1} type={igKey} />;
-            });
-        })
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
         .reduce((arr, el) => {
-             return arr.concat(el);
-        }, []); //flattening an array //advanced
-
-        if(tranfromedIngredients.length === 0){
-            tranfromedIngredients= <p>Please start adding ingredients</p>;
-        }
-
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            {tranfromedIngredients}
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,12 +6,15 @@ import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
-
-const sideDrawer = (props) => {
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
         <Aux>
-            <Backdrop show/>
-            <div className={classes.SideDrawer}>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
@@ -21,6 +24,6 @@ const sideDrawer = (props) => {
             </div>
         </Aux>
     );
-}
+};
 
 export default sideDrawer;
