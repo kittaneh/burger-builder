3af54bd67b429da3fde2db2c902985098f6acf3c
Yahya Daoud <kittaneh@gmail.com>
2020-07-25 12:21:47 +0300

    burger 2

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,17 +6,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-    const tranfromedIngredients = Object.keys(props.ingredients)
-      .map(igKey => {
-          return [...Array(props.ingredients[igKey])].map((_,i) => {
-            return <BurgerIngredient key= {igKey + 1} type={igKey} />;
-          }); 
-      });
+    let tranfromedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + 1} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+             return arr.concat(el);
+        }, []); //flattening an array //advanced
+
+        if(tranfromedIngredients.length === 0){
+            tranfromedIngredients= <p>Please start adding ingredients</p>;
+        }
 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-             {tranfromedIngredients}
+            {tranfromedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
