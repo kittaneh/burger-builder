188329818f79ed7095807c76e481dcf39b20ac29
Yahya Daoud <kittaneh@gmail.com>
2020-07-25 13:26:39 +0300

    dynamic add

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,18 +4,22 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label:'Salad', type:'salad'},
-    {label:'Bacon', type:'bacon'},
-    {label:'Cheese', type:'sheese'},
-    {label:'Meat', type:'meat'}
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
 ];
 
 const buildControls = (props) => (
-   <div className={classes.BuildControls}>
-     {controls.map((ctrl) => {
-         return <BuildControl key={ctrl.label} label={ctrl.label} />;
-     })}
-   </div>
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl) => {
+      return <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={ () => props.ingredientAdded(ctrl.type)}
+      />;
+    })}
+  </div>
 );
 
 export default buildControls;
