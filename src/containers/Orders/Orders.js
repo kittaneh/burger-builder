629e1ff0d1433a48f86b8741e6215a891c3ce66e
Yahya Daoud <kittaneh@gmail.com>
2020-07-27 16:14:55 +0300

    finishing order

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,17 +1,47 @@
 import React from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 
-export default class Orders extends React.Component {
+class Orders extends React.Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                let fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ orders: fetchedOrders, loading: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false });
+            });
+    }
 
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map((order, key) => {
+                    return <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                })}
             </div>
         );
     }
 
 }
 
+
+export default Orders;
